27636c5f3aeaeddf8537b19167cc085abeaf9e02
Khushi Prajapati <118883963+Khushi735@users.noreply.github.com>
2024-06-10 19:18:11 +0530

    Add files via upload

---
+++ Youtube-Clone/script.js
@@ -0,0 +1,6 @@
+const darkModeElement = document.getElementById('dark-mode');
+const bodyElement = document.body;
+
+darkModeElement.addEventListener('click', () => {
+  bodyElement.classList.toggle('dark-mode');
+});
