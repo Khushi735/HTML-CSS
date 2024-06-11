99da4eab1a3c45f5664eb611c892ca973096682c
Khushi Prajapati <118883963+Khushi735@users.noreply.github.com>
2024-06-11 13:55:38 +0530

    script.js

--- Youtube-Clone/script.js
+++ Youtube-Clone/script.js
@@ -1,6 +1,8 @@
-const darkModeElement = document.getElementById('dark-mode');
-const bodyElement = document.body;
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModeElement = document.getElementById('dark-mode');
+  const bodyElement = document.body;
 
-darkModeElement.addEventListener('click', () => {
-  bodyElement.classList.toggle('dark-mode');
+  darkModeElement.addEventListener('click', () => {
+    bodyElement.classList.toggle('dark-mode');
+  });
 });
